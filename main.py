ddb6480feb76b30e39da1c59a2a66c435ce02852
adrianpdm <adrian.padmodihardjo@gmail.com>
2023-08-14 04:59:54 +0700

    improve payload and filters
    - `context` is separated into `data`, `header`, and `footer` for readability and predictability
    - added utils to template globals

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from typing import Union, Dict, Optional
-from pydantic import BaseModel
 import uvicorn
+import utils
+
+from inspect import getmembers, isfunction
+from typing import Dict
+from pydantic import BaseModel
 from enum import Enum
 from fastapi import FastAPI, Request, Response, HTTPException, Body
 from fastapi.templating import Jinja2Templates
@@ -20,8 +23,10 @@ class PrintFormat(str, Enum):
 class PrintPayload(BaseModel):
     type: PrintType
     format: PrintFormat
-    context: Dict = {}
     filename: str
+    data: Dict = {}
+    header: Dict = {}
+    footer: Dict = {}
 
 @app.post("/print")
 async def handle_print(
@@ -30,29 +35,33 @@ async def handle_print(
 ):
     try:
         templates_dir = Jinja2Templates(directory='./templates')
+        templates_dir.env.globals = {
+            **templates_dir.env.globals,
+            **(dict(getmembers(utils, isfunction)))
+        }
         template_file = templates_dir.get_template(name=f"{body.type}.html")
-
-        # for debugging
-        context = body.context
-        if (body.type == PrintType.Debug):
-            context = { 'context': body.context }
 
         html_content = template_file.render({
             'request': request,
             'filename': body.filename,
-            **context,
+            'data': body.data,
+            'header': body.header,
+            'footer': body.footer,
         })
         
         if (body.format == PrintFormat.HTML):
+            filename = body.filename if body.filename.endswith('.html') else f"{body.filename}.html"
             return Response(
                 content=html_content,
                 headers={
                     'content-type': 'text/html',
+                    'content-disposition': 'Attachment',
+                    'filename': filename
                 }
             )
         if (body.format == PrintFormat.PDF):
             pdf_content = None
-            pdf_filename = body.filename if body.filename.endswith('.pdf') else f"{body.filename}.pdf"
+            filename = body.filename if body.filename.endswith('.pdf') else f"{body.filename}.pdf"
             async with async_api.async_playwright() as p:
                 browser = await p.chromium.launch(
                     headless=True
@@ -68,7 +77,7 @@ async def handle_print(
                     headers={
                         'content-type': 'application/pdf',
                         'content-disposition': 'Attachment',
-                        'filename': pdf_filename
+                        'filename': filename
                     }
                 )
         raise TypeError('invalid format')
